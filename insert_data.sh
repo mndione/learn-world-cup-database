74c679fe4dac03edd72c4412fedee1ba22d8a0ac
Mamadou Ndione <mndione@gmail.com>
2024-11-22 17:18:42 +0000

    Save progress: 1.1

--- insert_data.sh
+++ insert_data.sh
@@ -8,3 +8,36 @@ else
 fi
 
 # Do not change code above this line. Use the PSQL variable above to query your database.
+# WINNER=France
+echo $($PSQL "TRUNCATE games, teams")
+#$PSQL "SELECT team_id FROM teams WHERE name = '$WINNER';"
+#$PSQL "INSERT INTO teams(name) VALUES('$WINNER');"
+
+#OLDIFS=$IFS
+
+cat games.csv | while IFS="," read YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS
+do
+#echo $YEAR
+if [[ $YEAR != "year" ]]
+then
+   WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE name = '$WINNER'")
+   if [[ -z $WINNER_ID ]]
+   then
+    $PSQL "INSERT INTO teams(name) VALUES('$WINNER')"
+    # echo  $WINNER_INSERT
+    WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE name = '$WINNER'")
+    #echo $WINNER_ID: $WINNER
+   fi
+
+   OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE name = '$OPPONENT'")
+   if [[ -z $OPPONENT_ID ]]
+   then
+    $PSQL "INSERT INTO teams(name) VALUES('$OPPONENT')"
+    # echo  $WINNER_INSERT
+    OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE name = '$OPPONENT'")
+    #echo $WINNER_ID: $WINNER
+   fi
+
+   $PSQL "INSERT INTO games(year,round,winner_id,opponent_id,winner_goals,opponent_goals) VALUES($YEAR,'$ROUND',$WINNER_ID,$OPPONENT_ID,$WINNER_GOALS,$OPPONENT_GOALS)"
+fi
+done
